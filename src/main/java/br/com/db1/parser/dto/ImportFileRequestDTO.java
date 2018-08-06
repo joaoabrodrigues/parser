33ae90a5b7a906ec1cc2bb4e6519b00dd0f5c723
joaoabrodrigues <joaoabrodrigues@outlook.com>
2018-08-06 13:17:24 -0300

    update pom and adding @Setter annotation

--- src/main/java/br/com/db1/parser/dto/ImportFileRequestDTO.java
+++ src/main/java/br/com/db1/parser/dto/ImportFileRequestDTO.java
@@ -2,10 +2,12 @@ package br.com.db1.parser.dto;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @RequiredArgsConstructor
 public class ImportFileRequestDTO {
 
     @Getter
+    @Setter
     private String path;
 }
